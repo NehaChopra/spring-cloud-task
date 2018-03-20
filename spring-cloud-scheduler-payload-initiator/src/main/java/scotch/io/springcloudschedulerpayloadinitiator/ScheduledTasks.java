bd1d7537396a8e99361f866915bdc0a7a0476525
Neha Chopra <neha.chopra@navyuginfo.com>
2018-03-20 14:54:08 +0530

    Update ScheduledTasks.java

--- spring-cloud-scheduler-payload-initiator/src/main/java/scotch/io/springcloudschedulerpayloadinitiator/ScheduledTasks.java
+++ spring-cloud-scheduler-payload-initiator/src/main/java/scotch/io/springcloudschedulerpayloadinitiator/ScheduledTasks.java
@@ -51,8 +51,6 @@ public class ScheduledTasks {
 		String uri = "maven://scotch.io:logger-batch-task:jar:0.0.1-SNAPSHOT";
 		
 		List<String> commandArgsList = new ArrayList<>();
-//		commandArgsList.add("TaskRequest");
-//		commandArgsList.add("Arguments.......");
 		Map<String, String> deploymentProperties = new HashMap<String, String>();
 //			deploymentProperties.put("javaOpts", "-Xms152m");
 //			deploymentProperties.put("spring.cloud.deployer.nomad.javaOpts", "-Xms152m");
@@ -61,63 +59,8 @@ public class ScheduledTasks {
 		  commandArgsList.add(p.toString());
 		}
 		
-		commandArgsList.add("---"+jdbcTemplate.getDataSource().getConnection()+"---");
-		
-		logger.info("jdbcTemplate.getDataSource().getConnection()........."+jdbcTemplate.getDataSource().getConnection());
-		
-		logger.info("jdbcTemplate.getDataSource()........."+jdbcTemplate.getDataSource());
-		logger.info("jdbcTemplate.getDataSource() test another instance........."+jdbcTemplate.getDataSource());
-		
 		TaskLaunchRequest request = new TaskLaunchRequest(uri, commandArgsList, null, deploymentProperties, "TaskLauncher");
 		source.output().send(new GenericMessage<TaskLaunchRequest>(request));
 		logger.info("Finished the scheduled task Fixed Rate Task with Initial Delay :: Execution Time - {}", dateTimeFormatter.format(LocalDateTime.now()));
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-//		int i = 0;
-//		while(i == 0) {
-//		    logger.info("Fixed Rate Task with Initial Delay :: Execution Time - {}", dateTimeFormatter.format(LocalDateTime.now()));
-//			String uri = "maven://scotch.io:logger-batch-task:jar:0.0.1-SNAPSHOT";
-//			
-//			logger.info("Querying for customer records where first_name = 'Josh':");	
-//			jdbcTemplate.query(
-//	                "SELECT id, first_name, last_name FROM customers WHERE first_name = ?", new Object[] { "Josh" },
-//	                (rs, rowNum) -> new Customer(rs.getLong("id"), rs.getString("first_name"), rs.getString("last_name"))
-//	        ).forEach(customer -> logger.info(customer.toString()));
-//			
-//			
-//			List<String> commandArgsList = new ArrayList<>();
-//			commandArgsList.add("TaskRequest");
-//			commandArgsList.add("Arguments.......");
-//			Map<String, String> deploymentProperties = new HashMap<String, String>();
-////			deploymentProperties.put("javaOpts", "-Xms152m");
-////			deploymentProperties.put("spring.cloud.deployer.nomad.javaOpts", "-Xms152m");
-//			deploymentProperties.put("spring.cloud.deployer.memory", "152");
-//			TaskLaunchRequest request = new TaskLaunchRequest(uri, commandArgsList, null, deploymentProperties, "TaskLauncher");
-//			source.output().send(new GenericMessage<TaskLaunchRequest>(request));
-//			logger.info("Finished the scheduled task Fixed Rate Task with Initial Delay :: Execution Time - {}", dateTimeFormatter.format(LocalDateTime.now()));
-//			i++;
-//		}
 	}
 }
